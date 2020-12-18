e488ebf8ab44b4d9171d0fc9196787248a731a83
Ireneusz Trepka <irek.trepka@gmail.com>
2020-12-18 15:33:12 +0100

    user home writed

--- src/main/java/com/itrepka/photogallery/view/controllers/UserHomeViewController.java
+++ src/main/java/com/itrepka/photogallery/view/controllers/UserHomeViewController.java
@@ -1,13 +1,29 @@
 package com.itrepka.photogallery.view.controllers;
 
+import com.itrepka.photogallery.service.dto.PhotoDto;
+import com.itrepka.photogallery.service.exception.GalleryNotFoundException;
+import com.itrepka.photogallery.service.exception.PhotoNotFoundException;
+import com.itrepka.photogallery.service.exception.UserNotFoundException;
+import com.itrepka.photogallery.view.service.UserOperationsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class UserHomeViewController {
+    @Autowired
+    private UserOperationsService userOperationsService;
+
     @GetMapping("/home")
-    public ModelAndView displayAdminHome() {
-        return new ModelAndView("home");
+    public ModelAndView displayAdminHome() throws UserNotFoundException, GalleryNotFoundException, PhotoNotFoundException {
+        List<PhotoDto> photos = userOperationsService.getPhotos();
+        String galleryName = userOperationsService.getGalleryName();
+        ModelAndView mv = new ModelAndView("home");
+        mv.addObject("photos", photos);
+        mv.addObject("galleryName", galleryName);
+        return mv;
     }
 }
---
+++ src/main/java/com/itrepka/photogallery/view/service/UserOperationsService.java
@@ -0,0 +1,61 @@
+package com.itrepka.photogallery.view.service;
+
+import com.itrepka.photogallery.config.AuthenticationSystem;
+import com.itrepka.photogallery.model.User;
+import com.itrepka.photogallery.repository.UserRepository;
+import com.itrepka.photogallery.service.dto.GalleryDto;
+import com.itrepka.photogallery.service.dto.PhotoDto;
+import com.itrepka.photogallery.service.exception.GalleryNotFoundException;
+import com.itrepka.photogallery.service.exception.PhotoNotFoundException;
+import com.itrepka.photogallery.service.exception.UserNotFoundException;
+import com.itrepka.photogallery.service.services.GalleryService;
+import com.itrepka.photogallery.service.services.PhotoService;
+import com.itrepka.photogallery.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserOperationsService {
+    @Autowired
+    private AuthenticationSystem authenticationSystem;
+    @Autowired
+    private GalleryService galleryService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PhotoService photoService;
+
+
+    public List<PhotoDto> getPhotos() throws UserNotFoundException, GalleryNotFoundException, PhotoNotFoundException {
+        String name = authenticationSystem.getName();
+        User user = userRepository.findByLogin(name)
+                .orElseThrow(() -> new UserNotFoundException("Not found user with login = " + name));
+
+        Integer galleryId = user.getGallery().getGalleryId();
+        GalleryDto galleryDto = galleryService.getGalleryById(galleryId);
+        List<Integer> photosIds = galleryDto.getPhotosIds();
+        List<PhotoDto> photos = new ArrayList<>();
+        if (photosIds != null) {
+            for (Integer photoId : photosIds) {
+                PhotoDto photoDto = photoService.getPhotoById(photoId);
+                photos.add(photoDto);
+            }
+        }
+        return photos;
+    }
+
+    public String getGalleryName() throws GalleryNotFoundException, UserNotFoundException {
+        String name = authenticationSystem.getName();
+        User user = userRepository.findByLogin(name)
+                .orElseThrow(() -> new UserNotFoundException("Not found user with login = " + name));
+
+        Integer galleryId = user.getGallery().getGalleryId();
+
+        GalleryDto galleryDto = galleryService.getGalleryById(galleryId);
+
+        return galleryDto.getName();
+    }
+}
